74f500e2e3ef3295d9dd1bf8561acebe7641e351
yagya <yagyagoel87@gmail.com>
2024-02-06 02:23:45 +0530

    added the dashboard coponnents and made the send money component the fancy frontend bit is over

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
 
 const Dashboard = ()=>{
-
+    return<>
+<Appbar></Appbar>
+<Balance value="2020"></Balance>
+<Users></Users>
+</>
 }
 export default Dashboard;
