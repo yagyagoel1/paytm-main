f9613ac2249615dced9ba977986de2a40a8825ae
yagya <yagyagoel87@gmail.com>
2024-02-06 06:28:14 +0530

    fixed some bug

--- frontend/src/routes/SendMoney.jsx
+++ frontend/src/routes/SendMoney.jsx
@@ -46,7 +46,7 @@ const SendMoney = () => {
                     />
                     </div>
                     <button onClick= {()=>{
-                        axios.post("http://localhost:3000/api/v1/account/transfer",{to : {id},amount:{amount}},{headers})
+                        axios.post("http://localhost:3000/api/v1/account/transfer",{to : id,amount:amount},{headers})
                     }}className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Initiate Transfer
                     </button>
